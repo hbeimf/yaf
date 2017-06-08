23e8d099fcfbd78a1ad1c5ea22a49ef5a626d46e
maofeng <maofeng@jrq.com>
2017-06-08 16:42:39 +0800

    xx

--- public/js_src/socket_io.js
+++ public/js_src/socket_io.js
@@ -9,6 +9,36 @@ require([
     'socket.io'
 ],function(){
 
+    var userName = 'user' + Math.floor((Math.random()*1000)+1);
+
+    var socket =  io.connect();
+
+    socket.on('connect', function() {
+        output('<span class="connect-msg">Client has connected to the server!</span>');
+    });
+
+    socket.on('message', function(data) {
+        if (data instanceof Object) {
+            if (data.userName == 'image') {
+                // <img src="data:image/png;base64,这里放字符"/>
+                var con = '<img src="data:image/'+data.type+';base64,'+data.message+'"/>';
+                output('<span class="username-msg">' + data.userName + ':</span> ' + con + '<br /><br />');
+            } else {
+                var con = JSON.stringify(data);
+                output(con);
+            }
+
+        } else {
+            output(data);
+        }
+    });
+
+    socket.on('disconnect', function() {
+        output('<span class="disconnect-msg">The client has disconnected!</span>');
+    });
+
+
+
     function output(message) {
         var currentTime = "<span class='time'>" +  moment().format('HH:mm:ss.SSS') + "</span>";
         var element = $("<div>" + currentTime + "<font style=\"color:red;\">=></font>  " + message + "</div>");
@@ -32,40 +62,16 @@ require([
 
     $(function () {
 
-        var userName = 'user' + Math.floor((Math.random()*1000)+1);
 
-        var socket =  io.connect(':8088');
 
-        socket.on('connect', function() {
-            output('<span class="connect-msg">Client has connected to the server!</span>');
-        });
 
-        socket.on('message', function(data) {
-            if (data instanceof Object) {
-                if (data.userName == 'image') {
-                    // <img src="data:image/png;base64,这里放字符"/>
-                    var con = '<img src="data:image/'+data.type+';base64,'+data.message+'"/>';
-                    output('<span class="username-msg">' + data.userName + ':</span> ' + con + '<br /><br />');
-                } else {
-                    var con = JSON.stringify(data);
-                    output(con);
-                }
-
-            } else {
-                output(data);
-            }
-        });
-
-        socket.on('disconnect', function() {
-            output('<span class="disconnect-msg">The client has disconnected!</span>');
-        });
 
 
         console.log('socket io!');
         $('#request').click(function(){
             console.log('request');
             sendString();
-            SendWork();
+            // SendWork();
         });
 
         $('#clearConsole').click(function (){
