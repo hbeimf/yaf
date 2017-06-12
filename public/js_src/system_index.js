651a04cc33f1a944355a5d9703bc6efe46c1bfd0
maofeng <maofeng@jrq.com>
2017-06-12 13:05:46 +0800

    xx

--- public/js_src/system_index.js
+++ public/js_src/system_index.js
@@ -7,16 +7,15 @@ require([
     $(function () {
 		UITree.init();
 
+        $addMenu = $('#mod_900');
+
         $('.parent_menu').find('span').click(function(){
 
             var id = $(this).prop('id');
-            console.log(id);
-
             // 此处弹出编辑框
-            //
-
-            // $('#mod_900').modal('show');
-
+            var link = '/system/addMenu/id/'+id;
+            $addMenu.find('.modal-content').load(link);
+            $addMenu.modal('show');
         });
 	});
 });
