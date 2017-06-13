5d1a51ddce78966cc372a130dc897a3499babec0
maofeng <maofeng@jrq.com>
2017-06-13 10:47:57 +0800

    xx

---
+++ socketio/http_server/apps/doit/src/doit_sup.erl
@@ -0,0 +1,48 @@
+%%%-------------------------------------------------------------------
+%% @doc doit top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(doit_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+% init([]) ->
+%     {ok, { {one_for_all, 0, 1}, []} }.
+
+init([]) ->
+    Doit = {doit_server_tpl, {doit_server_tpl, start_link, []},
+               permanent, 5000, worker, [doit_server_tpl]},
+
+    FetchWeb = {doit_fetch_web, {doit_fetch_web, start_link, []},
+               permanent, 5000, worker, [doit_fetch_web]},
+
+    Children = [Doit, FetchWeb],
+
+    {ok, { {one_for_all, 10, 10}, Children} }.
+
+
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
