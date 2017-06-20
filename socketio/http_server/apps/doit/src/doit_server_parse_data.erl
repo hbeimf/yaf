ab5aac4498ca2671938c4a1c283d80bb38bc9ea7
maofeng <maofeng@jrq.com>
2017-06-20 09:33:50 +0800

    xx

---
+++ socketio/http_server/apps/doit/src/doit_server_parse_data.erl
@@ -0,0 +1,146 @@
+%% gen_server代码模板
+
+-module(doit_server_parse_data).
+
+-behaviour(gen_server).
+% --------------------------------------------------------------------
+% Include files
+% --------------------------------------------------------------------
+
+% --------------------------------------------------------------------
+% External exports
+% --------------------------------------------------------------------
+-export([]).
+
+% gen_server callbacks
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+% -record(state, {}).
+
+% --------------------------------------------------------------------
+% External API
+% --------------------------------------------------------------------
+-export([doit/0]).
+
+doit() ->
+    gen_server:cast(?MODULE, doit).
+
+
+
+% -export([start_goroutine/0, info/0, stop_goroutine/1, send_cast/2]).
+
+% info() ->
+%     gen_server:call(?MODULE, info).
+
+% start_goroutine() ->
+%     gen_server:call(?MODULE, start_goroutine).
+
+% stop_goroutine(GoMBox) ->
+%     gen_server:call(?MODULE, {stop_goroutine, GoMBox}).
+
+% send_cast(GoMBox, Msg) ->
+%     gen_server:cast(?MODULE, {send_cast, GoMBox, Msg}).
+
+% get_gombox() ->
+%     gen_server:call(?MODULE, get_gombox).
+
+% start_link() ->
+%     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+
+% --------------------------------------------------------------------
+% Function: init/1
+% Description: Initiates the server
+% Returns: {ok, State}          |
+%          {ok, State, Timeout} |
+%          ignore               |
+%          {stop, Reason}
+% --------------------------------------------------------------------
+init([]) ->
+    {ok, []}.
+
+% --------------------------------------------------------------------
+% Function: handle_call/3
+% Description: Handling call messages
+% Returns: {reply, Reply, State}          |
+%          {reply, Reply, State, Timeout} |
+%          {noreply, State}               |
+%          {noreply, State, Timeout}      |
+%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%          {stop, Reason, State}            (terminate/2 is called)
+% --------------------------------------------------------------------
+
+% handle_call({doit, FromPid}, _From, State) ->
+%     io:format("doit  !! ============== ~n~n"),
+
+%     lists:foreach(fun(_I) ->
+%         FromPid ! {from_doit, <<"haha">>}
+%     end, lists:seq(1, 100)),
+
+%     {reply, [], State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+% --------------------------------------------------------------------
+% Function: handle_cast/2
+% Description: Handling cast messages
+% Returns: {noreply, State}          |
+%          {noreply, State, Timeout} |
+%          {stop, Reason, State}            (terminate/2 is called)
+% --------------------------------------------------------------------
+% handle_cast({send_cast, GoMBox, Msg}, State) ->
+%     io:format("send cast !! ============== ~n~n"),
+%     % {ok, GoMBox} = application:get_env(go, go_mailbox),
+%     % io:format("message ~p!! ============== ~n~n", [GoMBox]),
+%     gen_server:cast(GoMBox, {Msg, self()}),
+%     {noreply, State};
+handle_cast(doit, State) ->
+    io:format("doit  !! ============== ~n~n"),
+
+    %% 此处将结果json缓存到数据库中，
+
+
+
+    {noreply, State};
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+% --------------------------------------------------------------------
+% Function: handle_info/2
+% Description: Handling all non call/cast messages
+% Returns: {noreply, State}          |
+%          {noreply, State, Timeout} |
+%          {stop, Reason, State}            (terminate/2 is called)
+% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+% handle_info(Info, State) ->
+%     % 接收来自go 发过来的异步消息
+%     io:format("~nhandle info BBB!!============== ~n~p~n", [Info]),
+%     {noreply, State}.
+
+% --------------------------------------------------------------------
+% Function: terminate/2
+% Description: Shutdown the server
+% Returns: any (ignored by gen_server)
+% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+% --------------------------------------------------------------------
+% Func: code_change/3
+% Purpose: Convert process state when code is changed
+% Returns: {ok, NewState}
+% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+% private functions
+
