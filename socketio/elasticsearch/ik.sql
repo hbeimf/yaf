7ba892a58985a3e16793ca89b3f62a91c4b6cf36
maofeng <maofeng@jrq.com>
2017-06-29 13:17:39 +0800

    xx

---
+++ socketio/elasticsearch/ik.sql
@@ -0,0 +1,62 @@
+http://blog.csdn.net/liuzhenfeng/article/details/39404435
+
+
+-- 1、到github网站下载源代码，网站地址为：
+
+https://github.com/medcl/elasticsearch-analysis-ik
+
+-- 右侧下方有一个按钮“Download ZIP"，点击下载源代码elasticsearch-analysis-ik-master.zip。
+
+
+
+
+
+
+
+
+mvn clean package
+
+
+
+
+
+-- http://www.cnblogs.com/xing901022/p/5910139.html
+-- http://www.cnblogs.com/xing901022/p/5910139.html
+-- http://blog.csdn.net/jam00/article/details/52983056
+
+
+-- https://github.com/medcl/elasticsearch-analysis-ik/releases?after=v1.10.1
+
+
+-rw-rw-r--  1 maomao maomao 27364449  8月 31  2016 elasticsearch-2.4.0.tar.gz
+-rw-rw-r--  1 maomao maomao   121402  6月 29 11:06 elasticsearch-analysis-ik-1.9.5.zip
+
+
+
+
+-- https://www.elastic.co/downloads/past-releases/elasticsearch-5-4-2
+https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.2.zip
+
+-- elasticsearch
+https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.2.tar.gz
+
+-- ik 分词
+https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v5.4.2/elasticsearch-analysis-ik-5.4.2.zip
+
+-- head 插件
+https://github.com/mobz/elasticsearch-head
+
+
+
+Running with built in server
+
+git clone git://github.com/mobz/elasticsearch-head.git
+cd elasticsearch-head
+npm install
+npm run start
+open http://localhost:9100/
+
+
+
+
+
