52fa1d3aa46b77a74e352459ac8d0f3bbdb318d5
maofeng <maofeng@jrq.com>
2017-06-30 16:21:25 +0800

    xx

--- socketio/http_server/apps/java/java_src/run.sh
+++ socketio/http_server/apps/java/java_src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -cp erlang-jinterface-1.7.1.jar:./ JInterfaceExample java_node@127.0.0.1 mailBox http_server_cookie
+java -cp erlang-jinterface-1.7.1.jar:./ JInterfaceExample java_node@127.0.0.1 javaMailBox http_server_cookie
 
 
 
