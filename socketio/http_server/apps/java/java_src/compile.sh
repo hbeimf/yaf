c18150bf9d347448487663685c8eaea75c08df7d
maofeng <maofeng@maofengtekiMacBook-Air.local>
2017-07-01 11:26:09 +0800

    xx

---
+++ socketio/http_server/apps/java/java_src/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mvn compile
+mvn assembly:assembly
+
+
+cp ./target/java-node-0.0.1-jar-with-dependencies.jar ../priv/
---
+++ socketio/http_server/apps/java/priv/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+java -jar java-node-0.0.1-jar-with-dependencies.jar java_node@localhost mailbox cookies
+
+
+
