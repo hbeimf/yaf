3cbfaecf7c6f01e0b5aa2c6212ccd24b68678f42
maofeng <maofeng@jrq.com>
2017-07-03 15:56:40 +0800

    xx

--- application/views/index/index.tpl
+++ application/views/index/index.tpl
@@ -17,7 +17,7 @@
 
                         <div class="span6">
 
-
+                            <div id="main"></div>
                         </div>
 
                         <div class="span6">
@@ -37,97 +37,4 @@
 
             <!-- END PAGE CONTAINER-->
 
-        </div>
-
-        <!-- END PAGE -->
-
-    </div>
-
-    <!-- END CONTAINER -->
-
-    <!-- BEGIN FOOTER -->
-
-    <div class="footer">
-
-        <div class="footer-inner">
-
-            2013 &copy; Metronic by keenthemes.Collect from <a href="http://www.cssmoban.com/" title="网站模板" target="_blank">网站模板</a> - More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a>
-
-        </div>
-
-        <div class="footer-tools">
-
-            <span class="go-top">
-
-            <i class="icon-angle-up"></i>
-
-            </span>
-
-        </div>
-
-    </div>
-
-    <!-- END FOOTER -->
-
-    <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-
-    <!-- BEGIN CORE PLUGINS -->
-
-    <script src="/js/jquery-1.10.1.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
-
-    <!-- IMPORTANT! Load jquery-ui-1.10.1.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-
-    <script src="/js/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-
-    <script src="/js/bootstrap.min.js" type="text/javascript"></script>
-
-    <!--[if lt IE 9]>
-
-    <script src="/js/excanvas.min.js"></script>
-
-    <script src="/js/respond.min.js"></script>
-
-    <![endif]-->
-
-    <script src="/js/jquery.slimscroll.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery.blockui.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery.cookie.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery.uniform.min.js" type="text/javascript" ></script>
-
-    <!-- END CORE PLUGINS -->
-
-    <!-- BEGIN PAGE LEVEL PLUGINS -->
-
-    <!-- END PAGE LEVEL PLUGINS -->
-
-    <!-- BEGIN PAGE LEVEL SCRIPTS -->
-
-    <script src="/js/app.js" type="text/javascript"></script>
-
-    <script src="/js/index.js" type="text/javascript"></script>
-
-    <!-- END PAGE LEVEL SCRIPTS -->
-
-    <script>
-
-        jQuery(document).ready(function() {
-
-           App.init(); // initlayout and core plugins
-
-
-        });
-
-    </script>
-
-    <!-- END JAVASCRIPTS -->
-
-</body>
-
-<!-- END BODY -->
-
-</html>
+{{include file="include/footer.tpl"}}
--- cache/compile_dir/0f2fa21ba47e3fb128ca86da055914572e14cde0.file.index.tpl.php
+++ cache/compile_dir/0f2fa21ba47e3fb128ca86da055914572e14cde0.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.8, created on 2017-07-03 15:19:00
+<?php /* Smarty version Smarty-3.1.8, created on 2017-07-03 15:55:57
          compiled from "/web/yaf/application/views/index/index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:1537775633593137f72c55a1-57778128%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '0f2fa21ba47e3fb128ca86da055914572e14cde0' => 
     array (
       0 => '/web/yaf/application/views/index/index.tpl',
-      1 => 1499066331,
+      1 => 1499068554,
       2 => 'file',
     ),
   ),
@@ -39,7 +39,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 
                         <div class="span6">
 
-
+                            <div id="main"></div>
                         </div>
 
                         <div class="span6">
@@ -59,98 +59,6 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 
             <!-- END PAGE CONTAINER-->
 
-        </div>
+<?php echo $_smarty_tpl->getSubTemplate ("include/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
-        <!-- END PAGE -->
-
-    </div>
-
-    <!-- END CONTAINER -->
-
-    <!-- BEGIN FOOTER -->
-
-    <div class="footer">
-
-        <div class="footer-inner">
-
-            2013 &copy; Metronic by keenthemes.Collect from <a href="http://www.cssmoban.com/" title="网站模板" target="_blank">网站模板</a> - More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a>
-
-        </div>
-
-        <div class="footer-tools">
-
-            <span class="go-top">
-
-            <i class="icon-angle-up"></i>
-
-            </span>
-
-        </div>
-
-    </div>
-
-    <!-- END FOOTER -->
-
-    <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-
-    <!-- BEGIN CORE PLUGINS -->
-
-    <script src="/js/jquery-1.10.1.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
-
-    <!-- IMPORTANT! Load jquery-ui-1.10.1.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-
-    <script src="/js/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-
-    <script src="/js/bootstrap.min.js" type="text/javascript"></script>
-
-    <!--[if lt IE 9]>
-
-    <script src="/js/excanvas.min.js"></script>
-
-    <script src="/js/respond.min.js"></script>
-
-    <![endif]-->
-
-    <script src="/js/jquery.slimscroll.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery.blockui.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery.cookie.min.js" type="text/javascript"></script>
-
-    <script src="/js/jquery.uniform.min.js" type="text/javascript" ></script>
-
-    <!-- END CORE PLUGINS -->
-
-    <!-- BEGIN PAGE LEVEL PLUGINS -->
-
-    <!-- END PAGE LEVEL PLUGINS -->
-
-    <!-- BEGIN PAGE LEVEL SCRIPTS -->
-
-    <script src="/js/app.js" type="text/javascript"></script>
-
-    <script src="/js/index.js" type="text/javascript"></script>
-
-    <!-- END PAGE LEVEL SCRIPTS -->
-
-    <script>
-
-        jQuery(document).ready(function() {
-
-           App.init(); // initlayout and core plugins
-
-
-        });
-
-    </script>
-
-    <!-- END JAVASCRIPTS -->
-
-</body>
-
-<!-- END BODY -->
-
-</html>
 <?php }} ?>
