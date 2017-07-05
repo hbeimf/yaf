aa7489c5720d831249da26bcae6856fd06859694
maofeng <maofeng@jrq.com>
2017-07-05 09:11:05 +0800

    xx

--- socketio/golang/HttpServer/HttpServer.go
+++ socketio/golang/HttpServer/HttpServer.go
@@ -1,3 +1,45 @@
-package main;
+package main
+
+// http://blog.csdn.net/su_sai/article/details/51176753
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "encoding/json"
+)
 
 
+type person struct {
+    NAME string
+    AGE  int
+}
+
+
+func main() {
+    http.HandleFunc("/test", hand)
+    err := http.ListenAndServe("127.0.0.1:8080", nil)
+    if err != nil {
+        fmt.Println("err")
+    }
+}
+
+
+func hand(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("ok")
+    w.Write([]byte("connect ok"))
+
+    defer r.Body.Close()
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        fmt.Println("read err")
+    }
+    fmt.Println(body)
+    tmp := person{}
+    err = json.Unmarshal(body, &tmp)  //JSON还原
+
+    if err != nil {
+        fmt.Println("unmarshal err")
+    }
+    fmt.Println(tmp)
+}
