12c26bd5cb6010d323a6c38951de23fd6204cd1d
maofeng <maofeng@jrq.com>
2017-07-05 09:27:12 +0800

    xx

--- socketio/golang/HttpServer/HttpServer.go
+++ socketio/golang/HttpServer/HttpServer.go
@@ -4,42 +4,66 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
+    // "io/ioutil"
     "net/http"
-    "encoding/json"
+    // "encoding/json"
 )
 
 
-type person struct {
-    NAME string
-    AGE  int
-}
+// type person struct {
+//     NAME string
+//     AGE  int
+// }
 
 
 func main() {
     http.HandleFunc("/test", hand)
+    http.HandleFunc("/get", get_handler)
+    http.HandleFunc("/post", post_handler)
+
     err := http.ListenAndServe("127.0.0.1:8080", nil)
     if err != nil {
         fmt.Println("err")
     }
 }
 
+// curl 127.0.0.1:8080/test
+func get_handler(w http.ResponseWriter, r *http.Request) {
+    defer r.Body.Close()
+
+    fmt.Println("ok")
+    w.Write([]byte("connect ok"))
+}
+
+// curl 127.0.0.1:8080/test
+func post_handler(w http.ResponseWriter, r *http.Request) {
+    defer r.Body.Close()
+
+    fmt.Println("ok")
+    w.Write([]byte("connect ok"))
+}
+
+
+
+
 
 func hand(w http.ResponseWriter, r *http.Request) {
+    defer r.Body.Close()
+
     fmt.Println("ok")
     w.Write([]byte("connect ok"))
 
-    defer r.Body.Close()
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        fmt.Println("read err")
-    }
-    fmt.Println(body)
-    tmp := person{}
-    err = json.Unmarshal(body, &tmp)  //JSON还原
 
-    if err != nil {
-        fmt.Println("unmarshal err")
-    }
-    fmt.Println(tmp)
+    // body, err := ioutil.ReadAll(r.Body)
+    // if err != nil {
+    //     fmt.Println("read err")
+    // }
+    // fmt.Println(body)
+    // tmp := person{}
+    // err = json.Unmarshal(body, &tmp)  //JSON还原
+
+    // if err != nil {
+    //     fmt.Println("unmarshal err")
+    // }
+    // fmt.Println(tmp)
 }
