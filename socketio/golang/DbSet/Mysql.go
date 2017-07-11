86adf1ae84275c9883ef0d0a7b96f60b4609d830
maofeng <maofeng@jrq.com>
2017-07-11 10:48:15 +0800

    xx

---
+++ socketio/golang/DbSet/Mysql.go
@@ -0,0 +1,35 @@
+package DbSet
+
+// https://github.com/go-xorm/xorm
+// https://www.kancloud.cn/kancloud/xorm-manual-zh-cn/56013
+import (
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/go-xorm/xorm"
+    "fmt"
+)
+
+var engine *xorm.Engine
+
+func mysqlEngine() (*xorm.Engine, error) {
+    return xorm.NewEngine("mysql", "root:123456@/test?charset=utf8")
+}
+
+func mysql_get() {
+    engine, err := mysqlEngine()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer engine.Close()
+    // test(engine)
+
+    results, err := engine.Query("select * from users")
+
+    fmt.Println("users2:", results)
+
+}
+
+
+
+
+
---
+++ socketio/golang/DbSet/Mysql_test.go
@@ -0,0 +1,30 @@
+package DbSet
+
+import (
+    "testing"
+    // "fmt"
+)
+
+// var obj = NewElasticSearch("127.0.0.1", "9200")
+
+func TestGet(t *testing.T) {
+
+
+    mysql_get()
+
+    // index := "db_100"
+    // table := "table_100"
+    // id := "id_100"
+
+    // fields := map[string]interface{}{"name": "xiaomin", "age": "10", "email":"123456@qq.com"}
+
+    // // var r map[string]interface{}
+    // obj.Insert(index, table, id, 30, fields)
+
+
+
+    // t.Log("getXXX", val, err)
+
+}
+
+
