961ce28c522605763abaa583276525b1decc18fd
maofeng <maofeng@jrq.com>
2017-07-25 15:19:06 +0800

    ws

--- socketio/newsproject/routes/index.js
+++ socketio/newsproject/routes/index.js
@@ -14,26 +14,11 @@ router.get('/', function(req, res, next) {
 
 
 
-router.get('/ws', function(req, res, next){
-  console.log('get route');
-  res.end();
-});
-
 // https://github.com/HenningM/express-ws
 router.ws('/ws', function(ws, req) {
-    util.inspect(ws);
   ws.on('message', function(msg) {
     ws.send(msg);
   });
-
-  // ws.on('connection', function connection(ws) {
-  //     ws.on('message', function incoming(message) {
-  //       console.log('received: %s', message);
-  //     });
-
-  //     ws.send('something');
-  // });
-
 });
 
 
