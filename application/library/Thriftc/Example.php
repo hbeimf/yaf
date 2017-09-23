79d445d5ec159a600c29925467e1da65c1a41613
mao <5622876@qq.com>
2017-09-23 05:08:30 -0700

    xx

--- application/library/Thriftc/Example.php
+++ application/library/Thriftc/Example.php
@@ -1,7 +1,46 @@
 <?php
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Protocol\TMultiplexedProtocol;
+use Thrift\Transport\TSocket;
+use Thrift\Transport\THttpClient;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Exception\TException;
+use com\penngo\RegisterServiceClient;
+use com\penngo\LoginServiceClient;
 
 class Thriftc_Example {
-	
+
+
+	function demo() {
+		try {
+		    // if (array_search('--http', $argv)) {
+		    //     //$socket = new THttpClient('localhost', 8080, '/php/PhpServer.php');
+		    // } else {
+		    //     $socket = new TSocket('localhost', 7911);
+		    // }
+
+		    $socket = new TSocket('localhost', 7911);
+		    // $transport = new TBufferedTransport($socket, 1024, 1024);
+		    // $protocol = new TBinaryProtocol($transport);
+		    // $loginProtocol = new TMultiplexedProtocol($protocol, "LoginService");
+		    // $registerProtocol = new TMultiplexedProtocol($protocol, "RegisterService");
+		    // $loginClient = new LoginServiceClient($loginProtocol);
+		    // $registerClient = new RegisterServiceClient($registerProtocol);
+		    // $transport->open();
+		    // $user = $loginClient->login('penngo', '123');
+		    // print "user===={$user->id} {$user->name} \n";
+		    
+		    // $user = $registerClient->createUser('test', '123456');
+		    // print "user===={$user->id} {$user->name} \n";
+		    
+		    $transport->close();
+		} catch (TException $tx) {
+		    print 'TException: '.$tx->getMessage()."\n";
+		    print 'TException: '.$tx->getTraceAsString()."\n";
+		}
+	}
+
+
 
 
 }
