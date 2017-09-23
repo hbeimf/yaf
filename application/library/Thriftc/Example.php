f28bd852b2fb8386512003b1c4d6bcfcee55873a
mao <5622876@qq.com>
2017-09-23 05:22:01 -0700

    xx

--- application/library/Thriftc/Example.php
+++ application/library/Thriftc/Example.php
@@ -1,45 +1,66 @@
 <?php
+
 use Thrift\Protocol\TBinaryProtocol;
-use Thrift\Protocol\TMultiplexedProtocol;
 use Thrift\Transport\TSocket;
 use Thrift\Transport\THttpClient;
 use Thrift\Transport\TBufferedTransport;
 use Thrift\Exception\TException;
 
-// use com\penngo\RegisterServiceClient;
-// use com\penngo\LoginServiceClient;
+
 use example\ExampleServiceClient;
+
+// http://thrift.apache.org/tutorial/php
 
 class Thriftc_Example {
 
 
 	function demo() {
 		try {
-		    // if (array_search('--http', $argv)) {
-		    //     //$socket = new THttpClient('localhost', 8080, '/php/PhpServer.php');
-		    // } else {
-		    //     $socket = new TSocket('localhost', 7911);
-		    // }
+			  // if (array_search('--http', $argv)) {
+			  //   $socket = new THttpClient('localhost', 8080, '/php/PhpServer.php');
+			  // } else {
+			    $socket = new TSocket('localhost', 9009);
+			  // }
+			  $transport = new TBufferedTransport($socket, 1024, 1024);
+			  $protocol = new TBinaryProtocol($transport);
+			  // $client = new \tutorial\CalculatorClient($protocol);
+			  $client = new \example\ExampleServiceClient($protocol);
 
-		    $socket = new TSocket('localhost', 9009);
-		    // $transport = new TBufferedTransport($socket, 1024, 1024);
-		    // $protocol = new TBinaryProtocol($transport);
-		    // $loginProtocol = new TMultiplexedProtocol($protocol, "LoginService");
-		    // $registerProtocol = new TMultiplexedProtocol($protocol, "RegisterService");
-		    // $loginClient = new LoginServiceClient($loginProtocol);
-		    // $registerClient = new RegisterServiceClient($registerProtocol);
-		    // $transport->open();
-		    // $user = $loginClient->login('penngo', '123');
-		    // print "user===={$user->id} {$user->name} \n";
-		    
-		    // $user = $registerClient->createUser('test', '123456');
-		    // print "user===={$user->id} {$user->name} \n";
+			  $transport->open();
 
-		    $transport->close();
-		} catch (TException $tx) {
-		    print 'TException: '.$tx->getMessage()."\n";
-		    print 'TException: '.$tx->getTraceAsString()."\n";
-		}
+			  $client->ping();
+			  print "ping()\n";
+
+			  // $sum = $client->add(1,1);
+			  // print "1+1=$sum\n";
+
+			  // $work = new \tutorial\Work();
+
+			  // $work->op = \tutorial\Operation::DIVIDE;
+			  // $work->num1 = 1;
+			  // $work->num2 = 0;
+
+			  // try {
+			  //   $client->calculate(1, $work);
+			  //   print "Whoa! We can divide by zero?\n";
+			  // } catch (\tutorial\InvalidOperation $io) {
+			  //   print "InvalidOperation: $io->why\n";
+			  // }
+
+			  // $work->op = \tutorial\Operation::SUBTRACT;
+			  // $work->num1 = 15;
+			  // $work->num2 = 10;
+			  // $diff = $client->calculate(1, $work);
+			  // print "15-10=$diff\n";
+
+			  // $log = $client->getStruct(1);
+			  // print "Log: $log->value\n";
+
+			  $transport->close();
+
+			} catch (TException $tx) {
+			  print 'TException: '.$tx->getMessage()."\n";
+			}
 	}
 
 
